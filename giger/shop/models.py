8cdc7d43481b69870e22d3c18b4e6e40a89e0944
DJmoster <Zapotichnyi12@gmail.com>
2023-05-20 20:57:41 +0300

    Спроектована база даних

--- giger/shop/models.py
+++ giger/shop/models.py
@@ -1,3 +1,100 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    '''Таблиця наявних товарів'''
+    category_id = models.ForeignKey('Categories', on_delete=models.PROTECT)
+    vendor_id   = models.ForeignKey('Vendors', on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=255)
+    sku  = models.CharField(max_length=255)
+
+    description_short = models.TextField()
+    description       = models.TextField()
+
+    # Під замовлення: -1; Немає в наявності: 0;
+    availability = models.IntegerField(default=-1)
+
+    is_active    = models.BooleanField(default=True)
+    price        = models.FloatField()
+
+    url_slug      = models.SlugField(max_length=255, db_index=True, unique=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+class Categories(models.Model):
+    '''Таблиця категорій та підкатегорій'''
+    parent_id  = models.ForeignKey('Categories', null=True, blank=True, on_delete=models.PROTECT)
+    hotline_id = models.IntegerField(null=True, blank=True)
+
+    name      = models.CharField(max_length=255)
+
+    url_slug  = models.SlugField(max_length=255, db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+
+class Vendors(models.Model):
+    '''Таблиця наявних вендорів'''
+    name = models.CharField(max_length=255)
+
+class ProductImages(models.Model):
+    '''Таблиця зображень товару'''
+    product_id = models.ForeignKey('Products', on_delete=models.CASCADE)
+    image      = models.ImageField(upload_to='product_photos/%Y/%m/')
+
+class ProductSales(models.Model):
+    '''Таблиця для знижок на товар'''
+    product_id = models.ForeignKey('Products', on_delete=models.CASCADE)
+    end_date   = models.DateTimeField()
+    price      = models.FloatField()
+
+class ProductReviews(models.Model):
+    '''Таблиця відгуків на товар'''
+    product_id = models.ForeignKey('Products', on_delete=models.CASCADE)
+    
+    name        = models.CharField(max_length=255)
+    email       = models.EmailField(max_length=255)
+    description = models.TextField()
+    rate        = models.IntegerField()
+
+class Customers(models.Model):
+    '''Таблиця клієнтів що робили замовлення'''
+    name    = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone   = models.CharField(max_length=15)
+
+
+class Orders(models.Model):
+    '''Таблиця замовлень'''
+    STATUSES = [
+        ('Н', 'НОВЕ'),
+        ('П', 'ПРИЙНЯТЕ'),
+        ('З', 'ЗАМОВЛЕНЕ'),
+        ('ВПД', 'В ПРОЦЕСІ ДОСТАВКИ'),
+        ('В', 'ВІДПРАВЛЕНЕ'),
+        ('ЗАВ', 'ЗАВЕРШЕНЕ'),
+    ]
+
+    customer_id = models.ForeignKey('Customers', on_delete=models.PROTECT)
+
+    status = models.CharField(max_length=255, choices=STATUSES, default='Н')
+    notes  = models.TextField()
+    shipping_method = models.CharField(max_length=255)
+
+    date_created = models.DateTimeField(auto_now=True, editable=False)
+
+class OrderItems(models.Model):
+    '''Таблиця товарів які людина замовила'''
+    product_id = models.ForeignKey('Products', blank=True, null=True, on_delete=models.SET_NULL)
+    order_id   = models.ForeignKey('Orders', on_delete=models.CASCADE)
+
+    name         = models.CharField(max_length=255)
+    buying_price = models.FloatField()
+    count        = models.IntegerField()
+
+class ItemWarranty(models.Model):
+    '''Таблиця наявних гарантій на товари'''
+    order_item_id = models.ForeignKey('OrderItems', on_delete=models.CASCADE)
+    serial_number = models.CharField(max_length=255)
+
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
